84b928d344033dd08e883ad68f79fcfc45614a57
Konrad Jaworski <konradx530@gmail.com>
2020-03-26 19:08:58 +0100

    work basic skeleton

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Coponents to serch for data
+
+class SearchBar extends React.Component {
+  render() {
+    return <div> Search bar</div>;
+  }
+}
+
+export default SearchBar;
