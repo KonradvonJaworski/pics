74b1a754e164abdfda41d59f553d40efa0efb414
Konrad Jaworski <konradx530@gmail.com>
2020-03-29 13:33:45 +0200

    add new method of transmission value in serach bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,7 @@ import React from 'react';
 // Coponents to serch for data
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -13,7 +11,10 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search </label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
